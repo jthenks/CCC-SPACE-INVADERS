148f6905f3973636bc5a1ea32b88df8116ea16fd
Peter Huang <phuan516@mtroyal.ca>
2022-11-02 14:55:44 -0600

    add enemies when level up

--- jordan-level.py
+++ jordan-level.py
@@ -294,6 +294,94 @@ class GameState():
 
         pygame.display.update()
 
+    def level_2(self):
+        global score_value
+        global playerX
+        global playerX_change
+        global bulletX
+        global bulletY
+        global bullet_state
+        global running
+
+        # Game Events
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    playerX_change = -3
+
+                if event.key == pygame.K_RIGHT:
+                    playerX_change = 3
+
+                if event.key == pygame.K_SPACE:
+                    if bullet_state == "ready":
+                        bullet_sound = pygame.mixer.Sound("./media/laser.wav")
+                        bullet_sound.play()
+                        bulletX = playerX
+                        fire_bullet(bulletX, bulletY)
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    playerX_change = 0
+
+        # Screen Attributes
+        screen.fill((0, 0, 0))
+        screen.blit(background, (0, 0))
+
+        playerX += playerX_change
+
+        if playerX <= 0:
+            playerX = 0
+        elif playerX >= 736:
+            playerX = 736
+
+        # Enemy Movement
+        for i in range(num_enemies):
+
+            # Game Over
+            if enemyY[i] > 440:  # trigger the end of the game
+                for j in range(num_enemies):
+                    enemyY[j] = 2000
+                game_over()
+                break
+
+            enemyX[i] += enemyX_change[i]
+            if enemyX[i] <= 0:
+                enemyX_change[i] = 4
+                enemyY[i] += enemyY_change[i]
+            elif enemyX[i] >= 736:
+                enemyX_change[i] = -4
+                enemyY[i] += enemyY_change[i]
+
+            collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
+            if collision:
+                explosion_sound = pygame.mixer.Sound("./media/explosion.wav")
+                explosion_sound.play()
+                bulletY = 480
+                bullet_state = "ready"
+                score_value += 1
+                enemyX[i] = random.randint(0, 800)
+                enemyY[i] = random.randint(50, 150)
+
+            enemy(enemyX[i], enemyY[i], i)
+
+        # Bullet Animation
+        if bulletY <= 0:
+            bulletY = 480
+            bullet_state = "ready"
+
+        if bullet_state == "fire":
+            fire_bullet(bulletX, bulletY)
+            bulletY -= bulletY_change
+
+        player(playerX, playerY)
+        show_score(textX, textY)
+
+        pygame.display.update()
+
     def state_manager(self):
         global num_enemies
 
@@ -301,7 +389,7 @@ class GameState():
             self.intro()
         if self.state == 'base_level':
             self.base_level()
-        if score_value > 1:
+        if score_value > 5:
             num_enemies = 12
             while len(enemyX) != 12:
                 enemyImg.append(pygame.image.load("./media/ufo.png"))
@@ -310,6 +398,15 @@ class GameState():
                 enemyX_change.append(4)
                 enemyY_change.append(40)
             self.level_2()
+        if score_value > 10:
+            num_enemies = 18
+            while len(enemyX) != 18:
+                enemyImg.append(pygame.image.load("./media/ufo.png"))
+                enemyX.append(random.randint(0, 735))
+                enemyY.append(random.randint(50, 150))
+                enemyX_change.append(4)
+                enemyY_change.append(40)
+            self.level_3()
 
 
 game_state = GameState()
