808bbff9538aac8c5582b4aae15475608334e993
Jordan <jordan.henkelman@gmail.com>
2022-11-03 23:33:00 -0600

    Got the different levels working, need to add score in between still

--- main.py
+++ main.py
@@ -28,7 +28,10 @@ enemyX = []
 enemyY = []
 enemyX_change = []
 enemyY_change = []
-num_enemies = 6
+num_enemies = 5
+num_enemies_lvl2 = 8
+num_enemies_lvl3 = 12
+num_enemies_lvl4 = 15
 
 
 # Bullet
@@ -112,7 +115,7 @@ class GameState():
 
         pygame.display.update()
 
-    def base_level(self):
+    def base_level(self, alien_num, alien_speed):
         global score_value
         global playerX
         global playerX_change
@@ -120,8 +123,9 @@ class GameState():
         global bulletY
         global bullet_state
         global running
+        global num_enemies
 
-        for i in range(num_enemies):
+        for i in range(alien_num):
             enemyImg.append(pygame.image.load("./media/ufo.png"))
             enemyX.append(random.randint(0, 735))
             enemyY.append(random.randint(50, 150))
@@ -175,10 +179,10 @@ class GameState():
 
             enemyX[i] += enemyX_change[i]
             if enemyX[i] <= 0:
-                enemyX_change[i] = 2
+                enemyX_change[i] = alien_speed
                 enemyY[i] += enemyY_change[i]
             elif enemyX[i] >= 736:
-                enemyX_change[i] = -2
+                enemyX_change[i] = -(alien_speed)
                 enemyY[i] += enemyY_change[i]
 
             collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
@@ -188,8 +192,11 @@ class GameState():
                 bulletY = 480
                 bullet_state = "ready"
                 score_value += 1
-                enemyX[i] = random.randint(0, 736)
-                enemyY[i] = random.randint(50, 150)
+                #enemyX[i] = random.randint(0, 736)
+                #enemyY[i] = random.randint(50, 150)
+                del enemyX[i]
+                del enemyY[i]
+                num_enemies -= 1
 
             enemy(enemyX[i], enemyY[i], i)
 
@@ -215,10 +222,10 @@ class GameState():
         global bulletY
         global bullet_state
         global running
+        global num_enemies_lvl2
 
-        num_enemies = 8
-        for i in range(num_enemies):
-            enemyImg.append(pygame.image.load("./media/ufo.png"))
+        for i in range(num_enemies_lvl2):
+            enemyImg.append(pygame.image.load("./media/level_4/ufo.png"))
             enemyX.append(random.randint(0, 736))
             enemyY.append(random.randint(50, 150))
             enemyX_change.append(3)
@@ -260,11 +267,11 @@ class GameState():
             playerX = 736
 
         # Enemy Movement
-        for i in range(num_enemies):
+        for i in range(num_enemies_lvl2):
 
             # Game Over
             if enemyY[i] > 440:  # trigger the end of the game
-                for j in range(num_enemies):
+                for j in range(num_enemies_lvl2):
                     enemyY[j] = 2000
                 game_over()
                 break
@@ -284,8 +291,110 @@ class GameState():
                 bulletY = 480
                 bullet_state = "ready"
                 score_value += 1
-                enemyX[i] = random.randint(0, 736)
-                enemyY[i] = random.randint(50, 150)
+                #enemyX[i] = random.randint(0, 736)
+                #enemyY[i] = random.randint(50, 150)
+                del enemyX[i]
+                del enemyY[i]
+                num_enemies_lvl2 -= 1
+
+            enemy(enemyX[i], enemyY[i], i)
+
+        # Bullet Animation
+        if bulletY <= 0:
+            bulletY = 480
+            bullet_state = "ready"
+
+        if bullet_state == "fire":
+            fire_bullet(bulletX, bulletY)
+            bulletY -= bulletY_change
+
+        player(playerX, playerY)
+        show_score(textX, textY)
+
+        pygame.display.update()
+
+    def level_3(self):
+        global score_value
+        global playerX
+        global playerX_change
+        global bulletX
+        global bulletY
+        global bullet_state
+        global running
+        global num_enemies_lvl3
+
+        for i in range(num_enemies_lvl3):
+            enemyImg.append(pygame.image.load("./media/ufo.png"))
+            enemyX.append(random.randint(0, 736))
+            enemyY.append(random.randint(50, 150))
+            enemyX_change.append(4)
+            enemyY_change.append(40)
+
+        # Game Events
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    playerX_change = -3
+
+                if event.key == pygame.K_RIGHT:
+                    playerX_change = 3
+
+                if event.key == pygame.K_SPACE:
+                    if bullet_state == "ready":
+                        bullet_sound = pygame.mixer.Sound("./media/laser.wav")
+                        bullet_sound.play()
+                        bulletX = playerX
+                        fire_bullet(bulletX, bulletY)
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    playerX_change = 0
+
+        # Screen Attributes
+        screen.fill((0, 0, 0))
+        screen.blit(background, (0, 0))
+
+        playerX += playerX_change
+
+        if playerX <= 0:
+            playerX = 0
+        elif playerX >= 736:
+            playerX = 736
+
+        # Enemy Movement
+        for i in range(num_enemies_lvl3):
+
+            # Game Over
+            if enemyY[i] > 440:  # trigger the end of the game
+                for j in range(num_enemies_lvl3):
+                    enemyY[j] = 2000
+                game_over()
+                break
+
+            enemyX[i] += enemyX_change[i]
+            if enemyX[i] <= 0:
+                enemyX_change[i] = 4
+                enemyY[i] += enemyY_change[i]
+            elif enemyX[i] >= 736:
+                enemyX_change[i] = -4
+                enemyY[i] += enemyY_change[i]
+
+            collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
+            if collision:
+                explosion_sound = pygame.mixer.Sound("./media/explosion.wav")
+                explosion_sound.play()
+                bulletY = 480
+                bullet_state = "ready"
+                score_value += 1
+                #enemyX[i] = random.randint(0, 736)
+                #enemyY[i] = random.randint(50, 150)
+                del enemyX[i]
+                del enemyY[i]
+                num_enemies_lvl3 -= 1
 
             enemy(enemyX[i], enemyY[i], i)
 
@@ -311,9 +420,9 @@ class GameState():
         global bulletY
         global bullet_state
         global running
+        global num_enemies_lvl4
 
-        num_enemies = 11
-        for i in range(num_enemies):
+        for i in range(num_enemies_lvl4):
             enemyImg.append(pygame.image.load("./media/level_4/ufo.png"))
             enemyX.append(random.randint(0, 736))
             enemyY.append(random.randint(50, 150))
@@ -356,21 +465,21 @@ class GameState():
             playerX = 736
 
         # Enemy Movement
-        for i in range(num_enemies):
+        for i in range(num_enemies_lvl4):
 
             # Game Over
             if enemyY[i] > 440:  # trigger the end of the game
-                for j in range(num_enemies):
+                for j in range(num_enemies_lvl4):
                     enemyY[j] = 2000
                 game_over()
                 break
 
             enemyX[i] += enemyX_change[i]
             if enemyX[i] <= 0:
-                enemyX_change[i] = 4
+                enemyX_change[i] = 5
                 enemyY[i] += enemyY_change[i]
             elif enemyX[i] >= 736:
-                enemyX_change[i] = -4
+                enemyX_change[i] = -5
                 enemyY[i] += enemyY_change[i]
 
             collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
@@ -380,8 +489,11 @@ class GameState():
                 bulletY = 480
                 bullet_state = "ready"
                 score_value += 1
-                enemyX[i] = random.randint(0, 736)
-                enemyY[i] = random.randint(50, 150)
+                #enemyX[i] = random.randint(0, 736)
+                #enemyY[i] = random.randint(50, 150)
+                del enemyX[i]
+                del enemyY[i]
+                num_enemies_lvl4 -= 1
 
             enemy(enemyX[i], enemyY[i], i)
 
@@ -406,17 +518,28 @@ class GameState():
         if self.state == 'intro':
             self.intro()
         if self.state == 'base_level':
-            self.base_level()
-        if score_value >= 3:
+            self.base_level(5, 2)
+        if score_value >= 5:
             self.state = 'level_2'
-            # put level up sound here
-            #score_value += 5
-            self.level_2()
-        if score_value >= 4:
-            self.state = 'level_4'
+            enemyImg.clear()
             # put level up sound here
             #score_value += 10
+            self.level_2()
+        if score_value >= 13:
+            self.state = 'level_3'
+            enemyImg.clear()
+            # put level up sound here
+            self.level_3()
+        if score_value >= 30:
+            self.state = 'level_4'
+            enemyImg.clear()
+            # put level up sound here
             self.level_4()
+        if score_value >= 57:
+            self.state = 'level_4'
+            enemyImg.clear()
+            # put level up sound here
+            # self.win()
 
 
 game_state = GameState()
